0a091140d5342cbf2c4457d6a5112378117c83e8
Ankit Shrestha <aankitshrestha@gmail.com>
2025-12-05 15:02:17 +0545

    feat: Add singleton db instance

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,11 +1,4 @@
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
+from app.core.db import get_chromadb
 
 def get_vectorstore():
-    embedding_function = GoogleGenerativeAIEmbeddings(
-        model = 'gemini-embedding-001',
-    )
-    vectorstore = Chroma(persist_directory='./vectorstore',
-                         embedding_function=embedding_function)
-    
-    return vectorstore
+    return get_chromadb()
---
+++ app/core/db.py
@@ -0,0 +1,15 @@
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+
+_vectordb = None
+
+def get_chromadb():
+    global _vectordb
+    if not isinstance(_vectordb, Chroma):
+        embedding_function = GoogleGenerativeAIEmbeddings(
+            model = 'gemini-embedding-001',
+        )
+        _vectordb = Chroma(persist_directory='./vectorstore',
+                            embedding_function=embedding_function)
+
+    return _vectordb
