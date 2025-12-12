9ac3a8a0c0870467acc8519c3508d5d8c7594e8b
Ankit Shrestha <aankitshrestha@gmail.com>
2025-12-12 15:17:17 +0545

    refactor: exception file changes

--- app/exception/__init__.py
+++ app/exception/__init__.py
@@ -1,10 +1,8 @@
 from app.exception.base import CustomError
-from app.exception.llm_provider import (
-    IngestionError,
-    QueryError,
-    VectorDBError,
-    LLMProviderError,
-)
+from app.exception.llm import LLMProviderError
+from app.exception.ingest import IngestionError
+from app.exception.query import QueryError
+from app.exception.vectordb import VectorDBError
 from app.exception.collection import CollectionAlreadyExistsError
 
 __all__ = (
---
+++ app/exception/query.py
@@ -0,0 +1,7 @@
+from app.exception import CustomError
+
+
+class QueryError(CustomError):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
